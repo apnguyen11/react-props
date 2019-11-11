1c5dbf1f544bc44d4d431bad7a03ce566e5c7233
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-10 18:55:04 -0600

    nametags complete

--- nameTags/App.js
+++ nameTags/App.js
@@ -4,11 +4,11 @@ const App = (props) => {
     return (
         <div className="d-flex flex-column align-items-center">
             {
-                nameTags.map((e, index) => {
-                    return <div class="nametag mx-auto w-50 text-center border m-1">
-                                <div class="hello bg-primary text-white p-3 h3">Hello, my name is:</div>
-                                <div class="name h4 p-5">e</div>
-                    key={index}</div>
+                nameTags.map((nameTag, index) => {
+                    return <div className="nametag mx-auto w-50 text-center border                 m-1" key={index}>
+                                <div className="hello bg-primary text-white p-3 h3">Hello, my name is:</div>
+                                <div className="name h4 p-5">{nameTag}</div>
+                        </div>
                 })
             }
         </div>
