50778240bba21ae210c13d2b73e6ce56fe7733e7
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-10 19:45:30 -0600

    poker cards generated

--- poker/App.js
+++ poker/App.js
@@ -1,11 +1,33 @@
-// Render the cards using React!
-const App = (props) => {
-    let { cards } = props;
+const Card = ({cardz})=>{
+    // let { card} = props;
+    console.log(cardz, "*********")
     return (
-        <span>cards</span>
-    )
+            <img style={{ width: 100 }} src={`cards/${cardz.value}${cardz.suit}.png`} />
+    );
 }
+
+
+
+
+
+// Render the cards using React!
+const App = props => {
+//   return (<Card cards={props}/>)
+let { cards } = props;
+// console.log(cards, '~~~~~~~~')
+return (
+    
+    <React.Fragment>
+        {cards.map((cardObject, index)=>  (
+            <Card cardz={cardObject} index={index}/>
+           
+            )
+        )}
+    </React.Fragment>
+)
+};
+
 
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
-const Card = null;  
+// const Card = null;  
