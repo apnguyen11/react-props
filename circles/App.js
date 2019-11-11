093a950cef6805e7a78d853293e168d771542216
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-10 18:48:28 -0600

    circles completed, nametags WIP

--- circles/App.js
+++ circles/App.js
@@ -1,8 +1,24 @@
+// import { realpathSync } from "fs";
+
 // Render the circles using React!
 const App = (props) => {
     let { circles } = props;
     return (
-        <span>circles</span>
+    
+        <div className="d-flex flex-column align-items-center">
+        {
+            circles.map(function (circle, index) {
+                console.log(circles)
+                return <div style={{
+                    width: (circle.radius * 2), 
+                    height: (circle.radius * 2), 
+                    borderRadius: circle.radius, 
+                    backgroundColor: circle.color
+                }}
+                key={index}></div>
+            })
+        }
+    </div>
     )
 }
 
--- nameTags/App.js
+++ nameTags/App.js
@@ -2,7 +2,16 @@
 const App = (props) => {
     let { nameTags } = props;
     return (
-        <span>name tags</span>
+        <div className="d-flex flex-column align-items-center">
+            {
+                nameTags.map((e, index) => {
+                    return <div class="nametag mx-auto w-50 text-center border m-1">
+                                <div class="hello bg-primary text-white p-3 h3">Hello, my name is:</div>
+                                <div class="name h4 p-5">e</div>
+                    key={index}</div>
+                })
+            }
+        </div>
     )
 }
 
